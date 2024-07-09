a4c47bdb27638aba0cfaefdbc5085ee8a4fc435f
Abujoory1 <abujoory1@example.com>
2024-07-09 07:58:39 -0700

    This script prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* This script prints 3 lines */
+const C = 'C is fun';
+const Python = 'Python is cool';
+const Js = 'JavaScript is amazing';
+console.log(C);
+console.log(Python);
+console.log(Js);
